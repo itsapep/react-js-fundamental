1c02743aa9b1ff18f38b0913543550fc34f32a35
Yurham Afif <yurhamafif.edu@gmail.com>
2022-07-27 09:11:41 +0700

    [add] create navbar.js

---
+++ src/component/navbar.js
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import { Container, NavbarBrand } from "react-bootstrap";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <>
+                <Navbar>
+                    <Container>
+                        <NavbarBrand>
+
+                        </NavbarBrand>
+                    </Container>
+                </Navbar>
+            </>
+        )
+    }
+}
+
+export default Navbar;
